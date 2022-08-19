6e12fb769cafb3d439fb39fb9cf061a8ceea7980
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-19 13:55:49 -0400

    Create Main.java
    The beginning.

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+/*
+	NOTES:
+		2 player game. or play with AI.
+		draw the board with 9 squares.
+		Make it so  each square cant get replaced by the other player or AI.
+		dont allow the player to enter anything else but one of the squares. 
+
+		each square will be its own method. Once used, it can not be reused until the game is restarted.
+		Make sure the game can get into a draw state.
+		
+		
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
