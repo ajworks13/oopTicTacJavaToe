4f5769e6d33157b30b1c72358b7ba7b0666bbbd3
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-21 18:57:47 -0400

    Update GameBoard.java
    Trying to use an array list as a way to limit on selecting a box during run time.

--- GameBoard.java
+++ GameBoard.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class GameBoard{
 	private String slot1 = " - ";
 	private String slot2 = " - ";
@@ -8,16 +10,41 @@ public class GameBoard{
 	private String slot7 = " - ";
 	private String slot8 = " - ";
 	private String slot9 = " - ";
+	private boolean gameOver = false;
 
+	public static ArrayList<Integer> slotsLeft;
 
+	public void theBoardList(){
+		slotsLeft = new ArrayList();
+		slotsLeft.add(1);
+	}
+	
+
+	public void howManySlotsLeft(){
+
+		for(int x : slotsLeft){
+			System.out.print(x);
+		}
+		System.out.println();
+	}
 
 	public void showBoard(){
-		//System.out.println("   |   |   ");
 		System.out.println(slot1 + "|" + slot2 + "|" + slot3);
 		System.out.println("------------");
 		System.out.println(slot4 + "|" + slot5 + "|" + slot6);
 		System.out.println("------------");
 		System.out.println(slot7 + "|" + slot8 + "|" + slot9);
-		//System.out.println("   |   |   ");
 	}
+
+	
+
+	// setter
+	public void insertSlot1(String theSlot1){
+		this.slot1 = theSlot1;
+		showBoard();
+		slotsLeft.remove(0);
+		
+	}
+
+	
 }
